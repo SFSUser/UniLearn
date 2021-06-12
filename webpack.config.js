5aefc60a9ccb5c85391cd1cf02c8d0eeeb33674c
Your Name <you@example.com>
2021-06-12 09:16:29 -0500

    First push

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: [
+    path.resolve(__dirname, 'src', 'tsc/index.tsx')
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: "development",
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?x?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.html/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './[name].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 8888
+  }
+};
