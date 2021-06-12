75fe32677b3ee3e15d5fc948611c9a433064c87b
Your Name <you@example.com>
2021-06-12 14:27:49 -0500

    Symfony & Webpack Encore

---
+++ assets/tsc/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './../scss/index.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import BasePage from './page/BasePage';
+
+console.log(styles);
+
+const root = document.getElementById('root');
+ReactDOM.render(<div>Perro</div>, root);
--- public/index.php
+++ public/index.php
@@ -1,1 +1,9 @@
-<h1>Hola mundo</h1>
+<?php
+
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
